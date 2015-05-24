b1cf1e2f4a3c9c8b7b81bbad1c783f71a90c6d8b
Calclavia <admin@calclavia.com>
2015-05-24 13:39:11 +0800

    Positioned now implements ComponentProvider

--- src/main/scala/com/resonant/core/prefab/block/Updater.scala
+++ src/main/scala/com/resonant/core/prefab/block/Updater.scala
@@ -4,7 +4,7 @@ package com.resonant.core.prefab.block
  * An object that can handle ticks.
  * @author Calclavia
  */
-trait Updater extends nova.core.world.component.Updater {
+trait Updater extends nova.core.component.Updater {
 
 	protected var ticks = 0L
 
