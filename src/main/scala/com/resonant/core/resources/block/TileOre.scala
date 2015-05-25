ff90aa53bcf03161b80798280ea9bf5f22d2c470
Calclavia <admin@calclavia.com>
2015-05-25 18:51:31 +0800

    More work migrating to component system

--- src/main/scala/com/resonant/core/resources/block/TileOre.scala
+++ src/main/scala/com/resonant/core/resources/block/TileOre.scala
@@ -3,11 +3,11 @@ package com.resonant.core.resources.block
 import java.util.Optional
 
 import com.resonant.core.resources.{Resource, ResourceFactory}
+import com.resonant.lib.wrapper.WrapFunctions._
 import nova.core.block.Block
-import nova.core.component.renderer.StaticRenderer
+import nova.core.block.component.StaticBlockRenderer
 import nova.core.render.Color
 import nova.core.render.model.{BlockModelUtil, Model}
-import nova.core.render.texture.Texture
 import nova.core.util.Direction
 
 /**
@@ -17,16 +17,16 @@ import nova.core.util.Direction
 class TileOre extends Block with Resource {
 	var renderingForeground = false
 
-	add(new StaticRenderer(this) {
-		override def renderStatic(model: Model) {
-			renderingForeground = false
-			BlockModelUtil.drawBlock(model, TileOre.this)
-			renderingForeground = true
-			BlockModelUtil.drawBlock(model, TileOre.this)
+	add(
+		new StaticBlockRenderer(this) {
+			override def renderStatic(model: Model) {
+				renderingForeground = false
+				BlockModelUtil.drawBlock(model, TileOre.this)
+				renderingForeground = true
+				BlockModelUtil.drawBlock(model, TileOre.this)
+			}
 		}
-	})
-
-	override def colorMultiplier(side: Direction): Color = if (renderingForeground) Color.argb(ResourceFactory.getColor(material)) else Color.white
-
-	override def getTexture(side: Direction): Optional[Texture] = if (renderingForeground) Optional.of(ResourceFactory.oreForeground) else Optional.of(ResourceFactory.oreBackground)
+			.setColorMultiplier((side: Direction) => if (renderingForeground) Color.argb(ResourceFactory.getColor(material)) else Color.white)
+			.setTexture(func((side: Direction) => if (renderingForeground) Optional.of(ResourceFactory.oreForeground) else Optional.of(ResourceFactory.oreBackground)))
+	)
 }
--- src/main/scala/com/resonant/wrapper/core/content/BlockCreativeBuilder.scala
+++ src/main/scala/com/resonant/wrapper/core/content/BlockCreativeBuilder.scala
@@ -3,14 +3,13 @@ package com.resonant.wrapper.core.content
 import java.util.Optional
 
 import com.resonant.core.structure.Structure
+import com.resonant.lib.wrapper.WrapFunctions._
 import com.resonant.wrapper.core.ResonantEngine
 import nova.core.block.Block
-import nova.core.block.component.Oriented
-import nova.core.entity.Entity
+import nova.core.block.Block.RightClickEvent
+import nova.core.block.component.{Oriented, StaticBlockRenderer}
 import nova.core.game.Game
 import nova.core.network.{Packet, PacketHandler}
-import nova.core.render.texture.Texture
-import nova.core.util.transform.vector.Vector3d
 import nova.core.util.{Category, Direction}
 
 object BlockCreativeBuilder {
@@ -21,12 +20,16 @@ class BlockCreativeBuilder extends Block with PacketHandler with Category {
 
 	add(new Oriented(this).setMask(0x3F))
 
+	add(new StaticBlockRenderer(this).setTexture(func((dir: Direction) => Optional.of(ResonantEngine.textureCreativeBuilder))))
+
+	rightClickEvent.add((evt: RightClickEvent) => onRightClick(evt))
+
 	/**
 	 * Called when the block is right clicked by the player
 	 */
-	override def onRightClick(entity: Entity, side: Int, hit: Vector3d): Boolean = {
-		Game.instance.guiFactory.showGui("creativeBuilder", entity, position)
-		return true
+	def onRightClick(evt: RightClickEvent) {
+		Game.instance.guiFactory.showGui("creativeBuilder", evt.entity, position)
+		evt.result = true
 	}
 
 	override def read(packet: Packet) {
@@ -41,8 +44,6 @@ class BlockCreativeBuilder extends Block with PacketHandler with Category {
 			})
 		}
 	}
-
-	override def getTexture(side: Direction): Optional[Texture] = Optional.of(ResonantEngine.textureCreativeBuilder)
 
 	override def getID: String = "creativeBuilder"
 
