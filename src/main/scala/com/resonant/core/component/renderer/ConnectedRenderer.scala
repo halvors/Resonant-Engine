1161f771b0fb3804fe45ac1f2cf14fa005d249d9
Calclavia <admin@calclavia.com>
2015-05-26 13:40:56 +0800

    Fixed all launch errors

--- src/main/scala/com/resonant/core/component/renderer/ConnectedRenderer.scala
+++ src/main/scala/com/resonant/core/component/renderer/ConnectedRenderer.scala
@@ -6,6 +6,7 @@ import com.resonant.lib.util.RotationUtility
 import com.resonant.wrapper.lib.wrapper.BitmaskWrapper._
 import nova.core.block.Block
 import nova.core.block.component.BlockCollider
+import nova.core.component.Require
 import nova.core.component.renderer.StaticRenderer
 import nova.core.render.model.{BlockModelUtil, Model, StaticCubeTextureCoordinates}
 import nova.core.render.texture.BlockTexture
@@ -14,24 +15,21 @@ import nova.core.util.Direction
 /**
  * A trait for blocks with connected textures.
  */
+@Require(classOf[BlockCollider])
 class ConnectedRenderer(block: Block, edgeTexture: BlockTexture) extends StaticRenderer(block) {
 
 	override def renderStatic(model: Model) {
 		//Render the block face
 		BlockModelUtil.drawBlock(model, block)
-		val opBounds = provider.getOp(classOf[BlockCollider])
+		//Render the block edge
+		val bounds = provider.get(classOf[BlockCollider]).getBoundingBox
+		for (dir <- Direction.DIRECTIONS; r <- 0 until 4) {
+			if (!sideMask.mask(dir)) {
+				val absDir = Direction.fromOrdinal(RotationUtility.rotateSide(dir.ordinal, r))
 
-		if (opBounds.isPresent) {
-			//Render the block edge
-			val bounds = opBounds.get.getBoundingBox
-			for (dir <- Direction.DIRECTIONS; r <- 0 until 4) {
-				if (!sideMask.mask(dir)) {
-					val absDir = Direction.fromOrdinal(RotationUtility.rotateSide(dir.ordinal, r))
-
-					if (!sideMask.mask(absDir)) {
-						val face = BlockModelUtil.drawDir(absDir, model, bounds.min.x, bounds.min.y, bounds.min.z, bounds.max.x, bounds.max.y, bounds.max.z, StaticCubeTextureCoordinates.instance)
-						face.texture = Optional.of(edgeTexture)
-					}
+				if (!sideMask.mask(absDir)) {
+					val face = BlockModelUtil.drawDir(absDir, model, bounds.min.x, bounds.min.y, bounds.min.z, bounds.max.x, bounds.max.y, bounds.max.z, StaticCubeTextureCoordinates.instance)
+					face.texture = Optional.of(edgeTexture)
 				}
 			}
 		}
