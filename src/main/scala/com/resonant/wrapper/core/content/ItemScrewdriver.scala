107e07ebc6b56e8ea493a21f96e15000ce923bff
Calclavia <admin@calclavia.com>
2015-06-01 01:01:01 +0800

    Fixed compiler errors

--- src/main/scala/com/resonant/wrapper/core/content/ItemScrewdriver.scala
+++ src/main/scala/com/resonant/wrapper/core/content/ItemScrewdriver.scala
@@ -1,22 +1,16 @@
 package com.resonant.wrapper.core.content
 
 import java.util.Optional
-import _root_.tools.IToolWrench
-import com.resonant.wrapper.core.{Content, ResonantEngine}
-import net.minecraft.entity.player.EntityPlayer
+
+import com.resonant.wrapper.core.Content
 import nova.core.component.Category
 import nova.core.item.Item
 import nova.core.render.texture.ItemTexture
 
-class ItemScrewdriver extends Item with IToolWrench {
+class ItemScrewdriver extends Item {
 	add(new Category("tools"))
 
 	override def getID: String = "screwdriver"
-
-	override def canWrench(entityPlayer: EntityPlayer, x: Int, y: Int, z: Int): Boolean = true
-
-	override def wrenchUsed(entityPlayer: EntityPlayer, x: Int, y: Int, z: Int) {
-	}
 
 	override def getTexture: Optional[ItemTexture] = Optional.of(Content.textureScrewdriver)
 }
