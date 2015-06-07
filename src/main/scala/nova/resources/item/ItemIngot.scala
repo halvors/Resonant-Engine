e1d685cf8af48900fadbc9baec279c378d56f7ff
Calclavia <admin@calclavia.com>
2015-06-07 23:09:11 +0800

    Fixed Quaternion fromDirection

--- src/main/scala/nova/resources/item/ItemIngot.scala
+++ src/main/scala/nova/resources/item/ItemIngot.scala
@@ -1,11 +1,7 @@
 package nova.resources.item
 
-import java.util.Optional
-
-import com.sun.prism.ResourceFactory
-import nova.resources.{Resource, ResourceFactory}
 import nova.core.item.Item
-import nova.core.render.texture.ItemTexture
+import nova.resources.Resource
 
 /**
  * @author Calclavia
